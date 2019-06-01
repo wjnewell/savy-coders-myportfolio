393bc760f53d711f21ca4387b04edfdd761429dc
wjnewell <wendijnewell@yahoo.com>
2019-06-01 11:54:20 -0500

    new file

---
+++ store/Links.js
@@ -0,0 +1,12 @@
+export default{
+    'primary': [
+        {'text':'Home'},
+        {'text': 'About'},
+        {'text': 'Contact'}
+    ]
+    'dropdown': [
+        {'text':'Showcase'},
+        {'text':'WebStore'},
+        {'text':'D-DayDemo}'
+    ]
+};
