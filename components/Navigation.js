924abf63df80398d17176aaabec38264868980de
wjnewell <wendijnewell@yahoo.com>
2019-06-02 15:33:40 -0500

    removed duplicate links, added build instructions for Netlify

--- components/Navigation.js
+++ components/Navigation.js
@@ -12,14 +12,14 @@ export default (state) =>
       <nav class = "menubar">
         <ul class= "display">
         ${buildNavHTML(state.links.primary)}
-          <li> <a href="about.html"> About </a></li>
-          <li> <a href="contact.html"> Contact </a></li>
+          <!--<li> <a href="about.html"> About </a></li>
+          <li> <a href="contact.html"> Contact </a></li>-->
           <li>Portfolio
         <ul id="dropdown">
         ${buildNavHTML(state.links.dropdown)}
-          <li><a href="showcase.html">Showcase</a></li>
+         <!-- <li><a href="showcase.html">Showcase</a></li>
           <li><a href="webstore.html">WebStore</a></li>
-          <li><a href="ddaydemo.html">D-DayDemo</a></li>
+          <li><a href="ddaydemo.html">D-DayDemo</a></li>-->
         </ul>
         </li>
       </ul>
